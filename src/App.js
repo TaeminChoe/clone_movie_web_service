8ba97e137e9a5e00b6aa3e61e83fdfb0759362e1
TaeminChoe <taeminteng@gmail.com>
2021-12-28 23:05:43 +0900

    update app useEffect ex

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react';
+
+function Hello() {
+	function destroyedFn() {
+		console.log('bye');
+	}
+	function effectFn() {
+		console.log("I'm here");
+		return destroyedFn;
+	}
+	useEffect(effectFn, []);
+	return <h1>Hello</h1>;
+}
+
 function App() {
+	const [showing, setShowing] = useState(false);
+	function onClick() {
+		setShowing(prev => !prev);
+	}
+
 	return (
 		<div className="App">
-			<h1>Welcome back!</h1>
+			{showing ? <Hello /> : null}
+			<button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
 		</div>
 	);
 }
