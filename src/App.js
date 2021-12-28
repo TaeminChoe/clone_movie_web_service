17d62a30024e1dfade6b825841ff2f8f950d3113
TaeminChoe <taeminteng@gmail.com>
2021-12-29 01:02:30 +0900

    update app coin list ex

--- src/App.js
+++ src/App.js
@@ -1,38 +1,64 @@
 import { useEffect, useState } from "react";
 
 function App() {
-	const [toDo, setToDo] = useState("");
-	const [toDos, setToDos] = useState([]);
-	function onSubmit(event) {
-		event.preventDefault();
-		if (toDo === "") {
-			return;
+	const [loading, setLoading] = useState(true);
+	const [coins, setCoins] = useState([]);
+	const [bit, setBit] = useState({});
+	const [usd, setUsd] = useState(0);
+	const [toBit, setToBit] = useState(0);
+
+	useEffect(() => {
+		fetch("https://api.coinpaprika.com/v1/ticker")
+			.then(res => res.json())
+			.then(json => {
+				setCoins(json);
+				setLoading(false);
+			});
+	}, []);
+
+	function onSelect(event) {
+		if (usd) {
+			const currBit = coins.find(coin => coin.id === event.target.value);
+			console.log(`use: ${usd}, name: ${bit.id}`);
+			setBit(currBit);
+			setToBit(usd / Number(currBit.price_usd));
 		}
-		setToDos(prev => [toDo, ...prev]);
-		setToDo("");
 	}
+
 	function onChange(event) {
-		setToDo(event.target.value);
+		setUsd(Number(event.target.value));
 	}
 
 	return (
 		<div className="App">
-			<h1>My To Dos {toDos.length}</h1>
-			<form onSubmit={onSubmit}>
-				<input
-					onChange={onChange}
-					value={toDo}
-					type="text"
-					placeholder="Write your to do..."
-				/>
-				<button>Add To Do</button>
-			</form>
-			<hr />
-			<ul>
-				{toDos.map((toDO, index) => (
-					<li key={index}>{toDO}</li>
-				))}
-			</ul>
+			<h1>Temm Bit! {loading ? "" : `(${coins.length})`}</h1>
+			{loading ? (
+				<span>Loading...</span>
+			) : (
+				<div>
+					<form
+						onSubmit={event => {
+							event.preventDefault();
+						}}
+					>
+						<input
+							onChange={onChange}
+							type="text"
+							placeholder="how much is it?"
+						/>
+						<span>USD to Bit: </span>
+						<span>{toBit}</span>
+					</form>
+					<select onChange={onSelect}>
+						<option default>Select coin</option>
+						{coins.map(coin => (
+							<option key={coin.id} value={coin.id}>
+								{coin.name} ({coin.symbol}): {coin.price_usd}
+							</option>
+						))}
+					</select>
+				</div>
+			)}
 		</div>
 	);
 }
